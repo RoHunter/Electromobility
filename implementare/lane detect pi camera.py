89d1c6d3a6768dbb12a3846ce165d3aa2d234f53
Barbieru George <g.barbieruu@gmail.com>
2020-02-22 16:07:38 +0200

    sings working

--- implementare/lane detect pi camera.py
+++ implementare/lane detect pi camera.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from comm import trimite_serial, detectat_crosswalk, detectat_fuel, detectat_stop, detectat_park
 import math
 from picamera.array import PiRGBArray
-from picamera import PiCamera	
-
+from picamera import PiCamera   
+CASCADE_ITEM = "semn" 
 inainte=b'f'
 stanga=b'l'
 dreapta=b'r'
@@ -17,13 +17,12 @@ stop=b's'
 mask_scale_factor = 0.01
 mask_width_factor = 0.5
 
-											#SEMNE#
+                                            #SEMNE#
 #############################################################################################################
-cascade_fuel = cv2.CascadeClassifier(fuel)
-cascade_stop = cv2.CascadeClassifier(stop)
-cascade_crosswalk = cv2.CascadeClassifier(crosswalk)
-cascade_park = cv2.CascadeClassifier(park)
-
+cascade_fuel = cv2.CascadeClassifier("fuel.xml")
+cascade_stop = cv2.CascadeClassifier("stop.xml")
+cascade_crosswalk = cv2.CascadeClassifier("crosswalk.xml")
+cascade_park = cv2.CascadeClassifier("park.xml")
 
 
 
@@ -52,7 +51,7 @@ def proceesed_img(original_image):
             img_width = original_image.shape[1]
             img_height = original_image.shape[0]
             mask_y = int(img_height*mask_scale_factor)
-            mask_x = int(img_width*mask_width_factor)		
+            mask_x = int(img_width*mask_width_factor)       
             proceesed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
             proceesed_img = cv2.GaussianBlur(proceesed_img,(5,5), 0)
             proceesed_img = cv2.Canny(proceesed_img, threshold1 =200, threshold2 = 120 )
@@ -61,170 +60,89 @@ def proceesed_img(original_image):
             proceesed_img = roi(proceesed_img, [polygons])
             return proceesed_img
 
-##this funtions sends the input to pic
-def straight():
-    trimite_serial(inainte)
-def little_left():
-	trimite_serial(stanga)
-def full_left():
-	trimite_serial(sstanga)
-
-def little_right():
-	trimite_serial(dreapta)
-def full_right():
-	trimite_serial(ddreapta)
-       
-def slow():
-	trimite_serial(stop)
-##last_time  = time.time()
 
 def detectie_semn_fuel():
-	if rectangles_fuel is not None:
-		time.sleep(0.01)
-		print('Detected fuel')
-		trimite_serial(detectat_fuel)
-	
+    if rectangles_fuel is not None:
+        time.sleep(0.01)
+        print('Detected fuel')
+        trimite_serial(detectat_fuel)
+    
 def detectie_semn_crosswalk():
-	if rectangles_crosswalk is not None:
-		time.sleep(0.01)
-		print('detected crosswalk')
-		trimite_serial('detectat_crosswalk')
-		
+    if rectangles_crosswalk is not None:
+        time.sleep(0.01)
+        print('detected crosswalk')
+        trimite_serial('detectat_crosswalk')
+        CASCADE_ITEM
 def detectie_semn_stop():
-	if rectangles_stop is not None:
-		time.sleep(0.01)
-		print('detected stop')
-		trimite_serial('detectat_stop')
-		
+    if rectangles_stop is not None:
+        time.sleep(0.01)
+        print('detected stop')
+        trimite_serial('detectat_stop')
+        
 def detectie_semn_park():
-	if rectangles_park is not None:
-		time.sleep(0.01)
-		print('detectat park')
-		trimite_serial('detectat_park')		
+    if rectangles_park is not None:
+        time.sleep(0.01)
+        print('detectat park')
+        trimite_serial('detectat_park')     
 
 camera = PiCamera()
 camera.resolution = (800, 608)
+camera.rotation = 180
 rawCapture = PiRGBArray(camera)
 time.sleep(0.01)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     screen =  frame.array
-    detectie_semn_crosswalk()
-    detectie_semn_stop()
-    detectie_semn_crosswalk()
-    detectie_semn_fuel()
-    new_image = proceesed_img(screen)
-    lines = cv2.HoughLinesP(new_image, 1 ,np.pi/180, 100,np.array([]), minLineLength = 50, maxLineGap = 15)
-    left_coordinate = []
-    right_coordinate = []
-      
-    if lines is not None:
-            for line in lines:
-                x1,y1,x2,y2 = line[0]
-                slope = (x2-x1)/(y2-y1)
-                if slope<0:
-                        left_coordinate.append([x1,y1,x2,y2])
-                elif slope>0:
-                        right_coordinate.append([x1,y1,x2,y2])
-            l_avg = np.average(left_coordinate, axis =0)
-            r_avg = np.average(right_coordinate, axis =0)
-            l =l_avg.tolist()
-            r = r_avg.tolist()
-            try:
-                  #with the finded slope and intercept, this is used to find the value of point x on both left and right line
-                  #the center point is denoted by finding center distance between two lines
-                  
-                c1,d1,c2,d2 = r
-                a1,b1, a2,b2 = l
-                l_slope = (b2-b1)/(a2-a1)
-                r_slope = (d2-d1)/(c2-c1)
-                l_intercept = b1 - (l_slope*a1)
-                r_intercept = d1 - (r_slope*c1)
-                y=360
-                l_x = (y - l_intercept)/l_slope
-                r_x = (y - r_intercept)/r_slope
-                distance = math.sqrt((r_x - l_x)**2+(y-y)**2)
-                #line_center repressent the center point on the line
-                line_center = distance/2
-                  
-                center_pt =[(l_x+line_center)]
-                f_r = [(l_x+(line_center*0.25))]
-                f_l = [(l_x+(line_center*1.75))]
-                  #create a center point which is fixed
-                center_fixed =[477]
-                x_1 = int(l_x)
-                x_2 = int(r_x)
-                '''The logic behind this code is simple,
-                the center_fixed should be in the center_line.
-                means the cars is in center of the lane, if its get away from center,
-                then the left and right functions are used accordingly,then if
-                the center fixed is too far from the center_pt the car takes complete left or right accordingly!'''
-                if center_pt==center_fixed:
-                    straight()
-                    print('forward')
-                elif center_pt > center_fixed and center_fixed > f_r:
-                    little_right()
-                    print('right')
-                elif center_pt < center_fixed and center_fixed < f_l:
-                    little_left()
-                    print('left')
-                elif center_fixed < f_r:
-                    full_right()
-                    print('full_ right')
-                elif center_fixed > f_l:
-                    full_left()
-                    print('full_left')
-                else:
-                    slow()
-                    print('slow')
-
-            except:
-                pass
-                straight()
-                print('FORWARD')
-
-
-    
-    line_image = display_line(screen,lines)
-    combo_image = cv2.addWeighted(screen,0.8, line_image,1.2,2)
-	#xxxxxxxxxxxxxxxxxxxxx fuel xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    rectangles_fuel = cascade_fuel.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=20,
-    			minSize=(75, 75))
+    #detectie_semn_crosswalk()
+    #detectie_semn_stop()
+    #detectie_semn_crosswalk()
+    #detectie_semn_fuel()
+    #xxxxxxxxxxxxxxxxxxxxx fuel xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    rectangles_fuel = cascade_fuel.detectMultiScale(screen, scaleFactor=1.3, minNeighbors=20,
+                minSize=(75, 75))
     for (i, (x, y, w, h)) in enumerate(rectangles_fuel):
-    	# Surround cascade with rectangle
-    	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    	cv2.putText(image, CASCADE_ITEM[0] + " #{}".format(i + 1), (x, y - 10),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-    		
-    		
+        # Surround cascade with rectangle
+        cv2.rectangle(screen, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        if rectangles_fuel is not None:
+            print('Fuel Detected')
+        #cv2.putText(screen, CASCADE_ITEM[0] + " #{}".format(i + 1), (x, y - 10),
+        #    cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+            
+            
     #xxxxxxxxxxxxxxxxxx stop xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    rectangles_stop = cascade_stop.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=20,
-    			minSize=(75, 75))		
+    rectangles_stop = cascade_stop.detectMultiScale(screen, scaleFactor=1.3, minNeighbors=20,
+                minSize=(75, 75))       
     for (i, (x, y, w, h)) in enumerate(rectangles_stop):
-    	# Surround cascade with rectangle
-    	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    	cv2.putText(image, CASCADE_ITEM[1] + " #{}".format(i + 1), (x, y - 10),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-    		
+        # Surround cascade with rectangle
+        cv2.rectangle(screen, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        if rectangles_stop is not None:
+            print('Stop Detected')
+        #cv2.putText(screen, CASCADE_ITEM[1] + " #{}".format(i + 1), (x, y - 10),
+        #    cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+            
     #xxxxxxxxxxxxxxxxxx crosswalk xxxxxxxxxxxxxxxxxxxxxxxx
     
-    rectangles_crosswalk = cascade_crosswalk.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=20,
-    			minSize=(75, 75))
+    rectangles_crosswalk = cascade_crosswalk.detectMultiScale(screen, scaleFactor=1.3, minNeighbors=20,
+                minSize=(75, 75))
     for (i, (x, y, w, h)) in enumerate(rectangles_crosswalk):
-    	# Surround cascade with rectangle
-    	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    	cv2.putText(image, CASCADE_ITEM[2] + " #{}".format(i + 1), (x, y - 10),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-    		
+        # Surround cascade with rectangle
+        cv2.rectangle(screen, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        if rectangles_crosswalk is not None:
+            print('Crosswalk Detected')
+        #cv2.putText(screen, CASCADE_ITEM[2] + " #{}".format(i + 1), (x, y - 10),
+        #    cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+            
     #################### park ###########################
-    rectangles_park = cascade_park.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=20,
-    			minSize=(75, 75))
+    rectangles_park = cascade_park.detectMultiScale(screen, scaleFactor=1.3, minNeighbors=20,
+                minSize=(75, 75))
     for (i, (x, y, w, h)) in enumerate(rectangles_park):
-    	# Surround cascade with rectangle
-    	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    	cv2.putText(image, CASCADE_ITEM[3] + " #{}".format(i + 1), (x, y - 10),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+        # Surround cascade with rectangle
+        cv2.rectangle(screen, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        if rectangles_park is not None:
+            print('Park Detected')
+        #cv2.putText(screen, CASCADE_ITEM[3] + " #{}".format(i + 1), (x, y - 10),
+        #   cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
 ############################################################################################################################################3
-    cv2.imshow('auto',cv2.cvtColor(combo_image, cv2.COLOR_BGR2RGB))
+    cv2.imshow('auto',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     rawCapture.truncate(0)
       
     if cv2.waitKey(25) & 0xff == ord('q'):
--- implementare/sign.py
+++ implementare/sign.py
@@ -1,43 +1,47 @@
-cascade_fuel = cv2.CascadeClassifier(fuel)
-cascade_stop = cv2.CascadeClassifier(stop)
-cascade_crosswalk = cv2.CascadeClassifier(crosswalk)
-cascade_park = cv2.CascadeClassifier(park)
+import cv2
+
+cascade_fuel = cv2.CascadeClassifier("fuel.xml")
+cascade_stop = cv2.CascadeClassifier("stop.xml")
+cascade_crosswalk = cv2.CascadeClassifier("crosswalk.xml")
+cascade_park = cv2.CascadeClassifier("park.xml")
 
 #xxxxxxxxxxxxxxxxxxxxx fuel xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 rectangles_fuel = cascade_fuel.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=20,
-			minSize=(75, 75))
+            minSize=(75, 75))
 for (i, (x, y, w, h)) in enumerate(rectangles_fuel):
-	# Surround cascade with rectangle
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	cv2.putText(image, CASCADE_ITEM[0] + " #{}".format(i + 1), (x, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-		
-		
+    # Surround cascade with rectangle
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.putText(image, CASCADE_ITEM[0] + " #{}".format(i + 1), (x, y - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+        
+        
 #xxxxxxxxxxxxxxxxxx stop xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 rectangles_stop = cascade_stop.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=20,
-			minSize=(75, 75))		
+            minSize=(75, 75))       
 for (i, (x, y, w, h)) in enumerate(rectangles_stop):
-	# Surround cascade with rectangle
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	cv2.putText(image, CASCADE_ITEM[1] + " #{}".format(i + 1), (x, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-		
+    # Surround cascade with rectangle
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.putText(image, CASCADE_ITEM[1] + " #{}".format(i + 1), (x, y - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+        
 #xxxxxxxxxxxxxxxxxx crosswalk xxxxxxxxxxxxxxxxxxxxxxxx
 
 rectangles_crosswalk = cascade_crosswalk.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=20,
-			minSize=(75, 75))
+            minSize=(75, 75))
 for (i, (x, y, w, h)) in enumerate(rectangles_crosswalk):
-	# Surround cascade with rectangle
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	cv2.putText(image, CASCADE_ITEM[2] + " #{}".format(i + 1), (x, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-		
+    # Surround cascade with rectangle
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.putText(image, CASCADE_ITEM[2] + " #{}".format(i + 1), (x, y - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+        
 #################### park ###########################
 rectangles_park = cascade_park.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=20,
-			minSize=(75, 75))
+            minSize=(75, 75))
 for (i, (x, y, w, h)) in enumerate(rectangles_park):
-	# Surround cascade with rectangle
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	cv2.putText(image, CASCADE_ITEM[3] + " #{}".format(i + 1), (x, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+    # Surround cascade with rectangle
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.putText(image, CASCADE_ITEM[3] + " #{}".format(i + 1), (x, y - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+
+
 
